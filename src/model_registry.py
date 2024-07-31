c29c0082d73e3d3663c8d3f11075f18f958dfa77
victornemenike <emenikevn@yahoo.com>
2024-07-31 12:10:09 +0200

    move all variables to main function

--- src/model_registry.py
+++ src/model_registry.py
@@ -4,21 +4,13 @@ import mlflow
 from mlflow.tracking import MlflowClient   
 from datetime import datetime 
 
-MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
-MLFLOW_EXPERIMENT_NAME = "fuel-price-experiment"
-
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
-
-client = MlflowClient(tracking_uri = MLFLOW_TRACKING_URI)
-
 
 def register_model(run_id, model_name):
     model_uri = f"runs:/{run_id}/model"
     mlflow.register_model(model_uri = model_uri, name = model_name)
 
 
-def transition_model(model_name, version, stage):
+def transition_model(client, model_name, version, stage):
     client.transition_model_version_stage(
         name = model_name,
         version = version,
@@ -37,12 +29,20 @@ def transition_model(model_name, version, stage):
 def main():
     model_name = "fuel-price-predictor"
     run_id = "f83b41f1ac2b450893bbcdaac0e5d157"
+
+    MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
+    MLFLOW_EXPERIMENT_NAME = "fuel-price-experiment"
+
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
+
+    client = MlflowClient(tracking_uri = MLFLOW_TRACKING_URI)
     register_model(run_id, model_name)
         
     latest_versions = client.get_latest_versions(name = model_name)
     version = latest_versions[-1].version
     stage = "Staging"
-    transition_model(model_name, version, stage)
+    transition_model(client, model_name, version, stage)
 
 if __name__ == '__main__':
     main()

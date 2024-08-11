41fc4d3a150330ada4ef6c228240f219760c84bd
victornemenike <emenikevn@yahoo.com>
2024-08-12 00:49:42 +0200

    add test_convert_to_timeseries

--- tests/unit_tests/test_data.py
+++ tests/unit_tests/test_data.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.abspath(os.path.join('..', 'src')))
 
 
 from data_processing import read_dataframe
+from data_processing import convert_to_timeseries
 import pandas as pd
 from deepdiff import DeepDiff
 
@@ -23,3 +24,18 @@ def test_read_dataframe():
     print(f'diff = {diff}')
 
     assert 'type_changes' not in diff
+
+def test_convert_to_timeseries():
+    data_path = '../data/2024_globus_gas_prices.parquet'
+    actual_df = read_dataframe(data_path)
+    actual_df = convert_to_timeseries(actual_df)
+
+    expected_df = read_dataframe(data_path)
+    expected_df['date'] = pd.to_datetime(expected_df['date'], utc=True)
+    expected_df.set_index('date', inplace=True)
+
+    actual_df_list_dicts = actual_df.to_dict('records')
+
+    expected_df_list_dicts = expected_df.to_dict('records')
+
+    assert actual_df_list_dicts == expected_df_list_dicts

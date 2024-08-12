f78abffd2cd57ee3f4045fe50c5842198a6606ca
victornemenike <emenikevn@yahoo.com>
2024-08-12 13:59:09 +0200

    reduce forecasting steps from 48 hrs to 2 hrs into the future

--- web-service/test_flask_app.py
+++ web-service/test_flask_app.py
@@ -4,7 +4,7 @@ import requests
 data_path = '../data/2024_val_data.parquet'
 data = pd.read_parquet(data_path)
 recent_data = data[-100:]
-num_forecast_steps = 48
+num_forecast_steps = 2
 
 
 # Convert DataFrame to dictionary

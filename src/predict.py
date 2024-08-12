918df7f6309cd63fe60a41fb8076da67217e9352
victornemenike <emenikevn@yahoo.com>
2024-08-12 15:16:18 +0200

    add local_model_forecast function

--- src/predict.py
+++ src/predict.py
@@ -4,7 +4,55 @@ import pandas as pd
 from data_processing import prepare_X_y
 from data_processing import generate_future_timestamps
 from plotting import plot_forecast
+import torch
 
+def local_model_forecast(model, data_name, data, num_forecast_steps):
+
+    X_val, _ = prepare_X_y(data_name, data, sequence_length=24)
+
+    # Define the number of future time steps to forecast
+    time_interval_min = 60 # @minute intervals
+
+    # Convert to NumPy and remove singleton dimensions
+    sequence_to_plot = X_val.squeeze().cpu().numpy()
+
+    # Use the last 24 data points as the starting point
+    historical_data = sequence_to_plot[-1]
+
+    # Initialize a list to store the forecasted values
+    forecasted_values = []
+
+    prediction_horizon = num_forecast_steps + len(historical_data)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Use the trained model to forecast future values
+    with torch.no_grad():
+        for _ in range(prediction_horizon):
+            # Prepare the historical_data tensor
+            historical_data_tensor = torch.as_tensor(historical_data).view(1, -1, 1).float().to(device)
+            # Use the model to predict the next value
+            predicted_value = model(historical_data_tensor).cpu().numpy()[0, 0]
+            
+
+            # Append the predicted value to the forecasted_values list
+            forecasted_values.append(predicted_value[0])
+
+            # Update the historical_data sequence by removing the oldest value and adding the predicted value
+            historical_data = np.roll(historical_data, shift=-1)
+            historical_data[-1] = predicted_value
+ 
+          
+    # Generate futute dates
+    last_timestamp = data.index[-1]
+
+    # Generate the next stipulated timepoints
+    future_timestamps = generate_future_timestamps(last_timestamp, num_forecast_steps, time_interval_min)
+
+    # Concatenate the original index with the future dates
+    combined_index = data.index.append(future_timestamps)
+
+    return prediction_horizon, future_timestamps, forecasted_values, combined_index, sequence_to_plot
 
 
 def forecast(model, data_name, data, num_forecast_steps):

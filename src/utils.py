342bd32ea5a8b106810874a58acb80bb5055430a
victornemenike <emenikevn@yahoo.com>
2024-08-14 14:53:07 +0200

    fixes from precommit hooks for src/util

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,34 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.tsa.stattools as ts
-import matplotlib.pyplot as plt
 
 
 def dftest(timeseries):
-    test = ts.adfuller(timeseries,)
-    dfoutput = pd.Series(test[0:4], 
-                         index=['Test Statistic','p-value','Lags Used','Observations Used'])
-    for key,value in test[4].items():
+    test = ts.adfuller(
+        timeseries,
+    )
+    dfoutput = pd.Series(
+        test[0:4], index=['Test Statistic', 'p-value', 'Lags Used', 'Observations Used']
+    )
+    for key, value in test[4].items():
         dfoutput[f'Critical Value ({key})%'] = value
     print(dfoutput)
-    #Determing rolling statistics
+    # Determing rolling statistics
     rolmean = timeseries.rolling(window=12).mean()
     rolstd = timeseries.rolling(window=12).std()
 
-    #Plot rolling statistics:
-    _ = plt.plot(timeseries, color='blue',label='Original')
+    # Plot rolling statistics:
+    _ = plt.plot(timeseries, color='blue', label='Original')
     _ = plt.plot(rolmean, color='red', label='Rolling Mean')
-    _ = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    _ = plt.plot(rolstd, color='black', label='Rolling Std')
     plt.legend(loc='best')
     plt.title('Rolling Mean and Standard Deviation')
     plt.grid()
     plt.show(block=False)
 
+
 def generate_future_timestamps(start_time, num_predictions, interval_hours):
-    future_times = pd.date_range(start=start_time, periods=num_predictions+1, freq=f'{interval_hours}T')[1:]
+    future_times = pd.date_range(
+        start=start_time, periods=num_predictions + 1, freq=f'{interval_hours}T'
+    )[1:]
     return future_times

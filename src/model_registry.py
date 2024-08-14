deab47d16ee067b9fff52ce350ad7e0efef0d3f4
victornemenike <emenikevn@yahoo.com>
2024-08-14 14:47:26 +0200

    fixes from precommit hooks for src/model_registry

--- src/model_registry.py
+++ src/model_registry.py
@@ -1,30 +1,31 @@
 # pylint: disable=duplicate-code
-from datetime import datetime 
 import warnings
-import mlflow
-from mlflow.tracking import MlflowClient  
-warnings.filterwarnings("ignore", category= FutureWarning)
+from datetime import datetime
 
+import mlflow
+from mlflow.tracking import MlflowClient
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 def register_model(run_id, model_name):
     model_uri = f"runs:/{run_id}/model"
-    mlflow.register_model(model_uri = model_uri, name = model_name)
+    mlflow.register_model(model_uri=model_uri, name=model_name)
 
 
 def transition_model(client, model_name, version, stage):
     client.transition_model_version_stage(
-        name = model_name,
-        version = version,
-        stage = stage,
-        archive_existing_versions = False
+        name=model_name,
+        version=version,
+        stage=stage,
+        archive_existing_versions=False,
     )
 
     date = datetime.today().date()
     client.update_model_version(
-        name = model_name,
-        version = version,
-        description = f"The model version {version} was transitioned to {stage} on {date}"
+        name=model_name,
+        version=version,
+        description=f"The model version {version} was transitioned to {stage} on {date}",
     )
 
 
@@ -38,13 +39,14 @@ def main():
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
 
-    client = MlflowClient(tracking_uri = MLFLOW_TRACKING_URI)
+    client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
     register_model(run_id, model_name)
-        
-    latest_versions = client.get_latest_versions(name = model_name)
+
+    latest_versions = client.get_latest_versions(name=model_name)
     version = latest_versions[-1].version
     stage = "Staging"
     transition_model(client, model_name, version, stage)
+
 
 if __name__ == '__main__':
     main()

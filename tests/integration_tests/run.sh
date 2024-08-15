e2a94747fb53c2d93e0f52caedc14aac54e1dc3f
victornemenike <emenikevn@yahoo.com>
2024-08-15 13:27:25 +0200

    modify run file in integration_test to match Makefile

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -2,11 +2,13 @@
 
 cd "$(dirname "$0")"
 
-LOCAL_TAG='date +"%Y-%m-%d-%H-%M"'
-LOCAL_IMAGE_NAME="fuel-price-prediction-service:${LOCAL_TAG}"
-
-
-docker build -t ${LOCAL_IMAGE_NAME} ..
+if ["${LOCAL_IMAGE_NAME}" == ""];then
+    LOCAL_TAG='date +"%Y-%m-%d-%H-%M"'
+    export LOCAL_IMAGE_NAME="fuel-price-prediction-service:${LOCAL_TAG}"
+    echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_TAG_NAME}"
+    docker build -t ${LOCAL_IMAGE_NAME} ..
+else:
+    echo "no need to build ${LOCAL_IMAGE_NAME}"
 
 docker run -d --rm -p 8080:8080 ${LOCAL_IMAGE_NAME}
 
